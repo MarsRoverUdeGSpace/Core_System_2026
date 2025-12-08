9215e74597545c2db6c318f9d092a4ca5438e4ab
Rodrigo Rea <rodrigo.rea@marsrover.mx>
2025-12-07 20:38:53 -0600

    feat(kukulcan-fw): add upio_ros2 firmware minimal RTE

--- kukulcan/firmware/upio-ros2/lib/RTE/RTE.h
+++ kukulcan/firmware/upio-ros2/lib/RTE/RTE.h
@@ -5,14 +5,12 @@
 #include <rcl/rcl.h>
 #include <rclc/rclc.h>
 #include <rclc/executor.h>
-#include <geometry_msgs/msg/twist.h>
 
 #define RCCHECK(fn) {rcl_ret_t temp_rc = fn; if((temp_rc != RCL_RET_OK)){}}
 
 typedef struct {
     rcl_publisher_t             publis;
     rcl_subscription_t          subs;
-    geometry_msgs__msg__Twist   twist_msg;
     rclc_executor_t             executor;
     rclc_support_t              support;
     rcl_allocator_t             allocator;
